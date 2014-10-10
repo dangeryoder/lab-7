f44f95f98f1f89dc934f5b8014e64006510b2f9b
dangeryoder <dangeryoder@gmail.com>
2014-10-10 15:15:32 -0500

    lab 7 finished

--- lab 7.py
+++ lab 7.py
@@ -9,10 +9,23 @@ while (odd <= 300):
 # 25pt
 
 List = ["ya", "ye", "yo", "yeah", "yea", "no", "na", "nah", "nope", "yep"]
+x = 0
+while(x < len(List)):
+    print List[x]
+    x = x + 1
+
+# 50pt
 import random
-rand = random.randstr(0, len(List) - 1)
-Loop = True
-while (Loop):
-    user = raw_input()
-    Loop = Loop + 1
-print List(rand)
+rand = random.randint(0,50)
+guess = 0
+while(guess!= rand):
+    print "Pick a number between 1 and 50"
+    guess = int(raw_input())
+    if guess > rand:
+        print "too high!"
+    elif guess < rand:
+        print "too low!"
+    elif guess == rand:
+        print "nice!"
+        
+        
